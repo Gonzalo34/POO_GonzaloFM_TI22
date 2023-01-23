bd4259db8ced046e63fd381fc35fe2778448949b
Gonzalo34 <122801077+Gonzalo34@users.noreply.github.com>
2023-01-23 14:55:39 +0000

    se actualizo la tabla de REDME.md

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Para las practicas llevadas a cabo en clase
 |N.|Descripcion|Archivo|
 
 |1|Hola mundo en python|Programa0.py|
+|2|Comentariorios multilinea, de linea y concateneacion|Programa1.py|
+
+
---
+++ semana2/programa1.py
@@ -0,0 +1,12 @@
+"""programa1
+   nombre = Gonzalo FM
+   fecha = 23/01/2023
+   descripcion...
+   En este codifo se va a conocer los comentarios multilinea y concateneacion
+"""
+v1 = "Hola" 
+v2 = "mundo python" # Las variables anteriores se utilizan para almacenar una cadena de caracteres
+
+print(v1 + v2)
+
+
